d50e1485d49364ae264403975a1e3b7acf3167c7
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2018-04-23 18:17:18 +0200

    FindPattern implemented

---
+++ FindPattern.m
@@ -0,0 +1,61 @@
+clear all; close all;
+
+colorThreshold = 80;
+numPeaks = 4; threshold = 0.5; fillGap = 5; minLength = 300;
+minRadius = 25; maxRadius = 35;
+
+I = imread('patternsTilted.bmp');
+try
+    grayI = rgb2gray(I);
+catch
+    grayI = I;
+end
+
+[centers, radii] = imfindcircles(grayI, [minRadius, maxRadius]);
+
+BW = grayI>colorThreshold;
+mascara=ones(9,9)/81;
+BW_Horiz=conv2(mascara,abs(diff(BW)));
+BW_Vert=conv2(mascara,abs(diff(BW')))';
+
+try
+BW_Vert = BW_Vert(1:length(BW_Horiz(:,1)),:);
+end
+try
+BW_Vert = BW_Vert(:,1:length(BW_Horiz(1,:)));
+end
+try
+BW_Horiz = BW_Horiz(1:length(BW_Vert(:,1)),:);
+end
+try
+BW_Horiz = BW_Horiz(:,1:length(BW_Vert(1,:)));
+end
+
+BW1 = BW_Horiz + BW_Vert;
+
+[H,T,R] = hough(BW1);
+P  = houghpeaks(H,numPeaks,'threshold',ceil(threshold*max(H(:))));
+x = T(P(:,2)); y = R(P(:,1));
+plot(x,y,'s','color','white');
+lines = houghlines(BW1,T,R,P,'FillGap', fillGap,'MinLength', minLength);
+
+imshow(grayI, 'InitialMagnification', 'fit'), hold on;
+
+viscircles(centers, radii, 'color', 'red');
+
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
